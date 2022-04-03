5da3bc1602257e0682dc1c0c673a4723cbd3c22c
kamilla0503 <kamilla.faizullina@yandex.ru>
2022-04-03 20:31:34 +0300

    0.001

--- Monte_Carlo.cpp
+++ Monte_Carlo.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
     //omp_set_num_threads(1);
     //Lattice_2D l(100);
     int N = std::stoi(argv[1]);
-    double J = 0.01*(double)std::stoi(argv[2]);
+    double J = 0.001*(double)std::stoi(argv[2]);
 
 
     //Monte_Carlo_on_SAWS m(3,N,J,10000,100000000,100);
