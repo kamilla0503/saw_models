ee58b70b46dfd1d1b7b9f7908915ec350efbafbc
kamilla0503 <kamilla.faizullina@yandex.ru>
2022-04-29 21:44:19 +0300

    for 3D

--- Monte_Carlo.cpp
+++ Monte_Carlo.cpp
@@ -103,9 +103,9 @@ int main(int argc, char *argv[])
     //Lattice_2D l(100);
     int N = std::stoi(argv[1]);
     double J = 0.001*(double)std::stoi(argv[2]);
-
+    std::cout << N << " " << J << std::endl;
     //Monte_Carlo_on_SAWS m(3,N,J,10000,100000000,100);
-    Monte_Carlo_on_SAWS m(2,N,J,20000,100000000000000,100000);
+    Monte_Carlo_on_SAWS m(3,N,J,20000,100000000000000,100);
     //ProfilerStart("main.prof");
     m.run_simulation();
     //ProfilerStop();
